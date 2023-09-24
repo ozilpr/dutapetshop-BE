2f55a346475a19315af56d39813d4090433a0498
ozilpr <ozilp48@gmail.com>
2023-09-25 06:34:30 +0700

    structuring directory and add exceptions

---
+++ src/serves.js
@@ -0,0 +1,18 @@
+const Hapi = require('@hapi/hapi')
+
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*']
+      }
+    }
+  })
+
+  await server.start()
+  console.log(`server berjalan pada ${server.info.uri}`)
+}
+
+init()
