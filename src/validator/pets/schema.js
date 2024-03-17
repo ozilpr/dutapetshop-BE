efce9ae800e8b73b17d56e1a53b631bd8f7460cb
ozilpr <ozilp48@gmail.com>
2024-03-17 16:10:33 +0700

    update pets payload schema

--- src/validator/pets/schema.js
+++ src/validator/pets/schema.js
@@ -2,10 +2,10 @@ const Joi = require('joi')
 
 const PetsPayloadSchema = Joi.object({
   name: Joi.string().required(),
-  type: Joi.string(),
-  race: Joi.string(),
-  gender: Joi.string(),
-  birthdate: Joi.string()
+  type: Joi.string().allow(null, ''),
+  race: Joi.string().allow(null, ''),
+  gender: Joi.string().allow(null, ''),
+  birthdate: Joi.string().allow(null, '')
 })
 
 module.exports = { PetsPayloadSchema }
