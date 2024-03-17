6faf4442cc484a2299e0261c427faac05d8dd1d8
ozilpr <ozilp48@gmail.com>
2024-03-17 16:10:19 +0700

    update owners payload schema

--- src/validator/owners/schema.js
+++ src/validator/owners/schema.js
@@ -1,9 +1,9 @@
 const Joi = require('joi')
 
 const OwnersPayloadSchema = Joi.object({
-  registedCode: Joi.string().required(),
+  registerCode: Joi.string().required(),
   name: Joi.string().required(),
-  phone: Joi.string().regex(/^[0-9]{15}$/)
+  phone: Joi.string().regex(/^[0-9]{12,15}$/).allow(null, '')
 })
 
 module.exports = { OwnersPayloadSchema }
