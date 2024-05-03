53459b0af0709d4ad9e0f3b5cfc5d6eb577141f6
ozilpr <ozilp48@gmail.com>
2024-05-04 00:27:44 +0700

    commented a line of code

--- src/utils/GetLocalTime.js
+++ src/utils/GetLocalTime.js
@@ -1,8 +1,9 @@
 class GetLocalTime {
   async getDate () {
     const utcTimeStamp = Date.now()
-    const gmt7Offset = 7 * 60 * 60 * 1000
-    const gmt7Timestamp = utcTimeStamp + gmt7Offset
+    // idk this is my laptop or the Date libs, sometimes i need to add + 7 hours to match gmt+7
+    // const gmt7Offset = 7 * 60 * 60 * 1000
+    const gmt7Timestamp = utcTimeStamp
     const date = new Date(gmt7Timestamp).toISOString()
 
     return date
