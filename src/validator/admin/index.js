bf1123a921b3ced087fbaed4b878d5237b35e782
ozilpr <ozilp48@gmail.com>
2024-05-04 00:28:18 +0700

    added update admin payload schema

--- src/validator/admin/index.js
+++ src/validator/admin/index.js
@@ -1,9 +1,13 @@
 const InvariantError = require('../../exceptions/InvariantError')
-const { AdminPayloadSchema } = require('./schema')
+const { AdminPayloadSchema, UpdateAdminPayloadSchema } = require('./schema')
 
 const AdminValidator = {
   validateAdminPayload: (payload) => {
     const validationResult = AdminPayloadSchema.validate(payload)
+    if (validationResult.error) throw new InvariantError(validationResult.error.message)
+  },
+  validateUpdateAdminPayload: (payload) => {
+    const validationResult = UpdateAdminPayloadSchema.validate(payload)
     if (validationResult.error) throw new InvariantError(validationResult.error.message)
   }
 }
