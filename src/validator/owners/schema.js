8905f117dd1a2af5bad94f5c98e1998cd061320b
ozilpr <ozilp48@gmail.com>
2024-05-04 00:29:12 +0700

    added custom validaton message

--- src/validator/owners/schema.js
+++ src/validator/owners/schema.js
@@ -3,7 +3,15 @@ const Joi = require('joi')
 const OwnersPayloadSchema = Joi.object({
   registerCode: Joi.string().required(),
   name: Joi.string().required(),
-  phone: Joi.string().regex(/^[0-9]{12,15}$/).allow(null, '')
+  phone: Joi.string()
+    .allow(null, '')
+    .regex(/^[0-9]{10,15}$/)
+    .messages({
+      'string.pattern.base': `
+        "Nomor telepon" dengan nilai "{#value}"
+        tidak sesuai pola yang diperlukan: harus berupa angka antara 10 hingga 15 digit.
+      `
+    })
 })
 
 module.exports = { OwnersPayloadSchema }
