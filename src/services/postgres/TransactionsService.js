dce25cafe626a11221f4fb07e3eef1c519a4d75a
ozilpr <ozilp48@gmail.com>
2024-05-10 13:30:15 +0700

    added trycatch

--- src/services/postgres/TransactionsService.js
+++ src/services/postgres/TransactionsService.js
@@ -38,81 +38,25 @@ class TransactionsService {
   }
 
   async getResourcePriceByResourceId (resourceId) {
-    const query = {
-      text: 'SELECT price FROM med_resources WHERE id = $1 AND deleted_at IS NULL',
-      values: [resourceId]
+    try {
+      const query = {
+        text: 'SELECT price FROM med_resources WHERE id = $1 AND deleted_at IS NULL',
+        values: [resourceId]
+      }
+
+      const result = await this._pool.query(query)
+
+      if (!result.rows[0].price) throw new NotFoundError('Resource id tidak ditemukan')
+
+      return result.rows[0].price
+    } catch (error) {
+      console.log(error)
     }
-
-    const result = await this._pool.query(query)
-
-    if (!result.rows[0].price) throw new NotFoundError('Resource id tidak ditemukan')
-
-    return result.rows[0].price
   }
 
   async getTransactionDetails () {
-    const transactionDetailResult = await this._pool.query(`
-      SELECT DISTINCT
-        td.id,
-        o.id AS owner_id,
-        o.name AS owner_name,
-        o.register_code,
-        td.transaction_date,
-        td.updated_at
-      FROM
-        transaction_details td
-        INNER JOIN transactions t ON td.id = t.transaction_id
-        INNER JOIN med_resources r ON t.resource_id = r.id
-        INNER JOIN owners o ON td.owner_id = o.id
-      WHERE 
-        td.deleted_at IS NULL
-        AND t.deleted_at IS NULL
-      ORDER BY
-        td.transaction_date
-      DESC
-    `)
-
-    const transactionsResult = await this._pool.query(`
-      SELECT DISTINCT
-        t.id,
-        t.transaction_id,
-        r.name AS resource_name,
-        t.quantity,
-        t.price
-      FROM
-        transaction_details td
-        INNER JOIN transactions t ON td.id = t.transaction_id
-        INNER JOIN med_resources r ON t.resource_id = r.id
-        INNER JOIN owners o ON td.owner_id = o.id
-      WHERE
-        td.deleted_at IS NULL
-        AND t.deleted_at IS NULL
-      ORDER BY
-        r.name
-    `)
-
-    const data = transactionDetailResult.rows.map(transactionDetail => {
-      const transactions = transactionsResult.rows.filter(transaction => transaction.transaction_id === transactionDetail.id)
-      return {
-        transaction_id: transactionDetail.id,
-        owner_id: transactionDetail.owner_id,
-        owner_name: transactionDetail.owner_name,
-        register_code: transactionDetail.register_code,
-        transaction_date: transactionDetail.transaction_date,
-        transactions: transactions.map(transaction => ({
-          id: transaction.id,
-          resource_name: transaction.resource_name,
-          quantity: transaction.quantity,
-          price: transaction.price
-        }))
-      }
-    })
-    return data
-  }
-
-  async getTransactionDetailById (id) {
-    const queryTransactionDetail = {
-      text: `
+    try {
+      const transactionDetailResult = await this._pool.query(`
         SELECT DISTINCT
           td.id,
           o.id AS owner_id,
@@ -125,22 +69,19 @@ class TransactionsService {
           INNER JOIN transactions t ON td.id = t.transaction_id
           INNER JOIN med_resources r ON t.resource_id = r.id
           INNER JOIN owners o ON td.owner_id = o.id
-        WHERE
-          td.id = $1 
-          AND td.deleted_at IS NULL
+        WHERE 
+          td.deleted_at IS NULL
           AND t.deleted_at IS NULL
         ORDER BY
           td.transaction_date
-      `,
-      values: [id]
-    }
+        DESC
+      `)
 
-    const queryTransactions = {
-      text: `
+      const transactionsResult = await this._pool.query(`
         SELECT DISTINCT
           t.id,
           t.transaction_id,
-          r.name AS resource_name, 
+          r.name AS resource_name,
           t.quantity,
           t.price
         FROM
@@ -149,38 +90,110 @@ class TransactionsService {
           INNER JOIN med_resources r ON t.resource_id = r.id
           INNER JOIN owners o ON td.owner_id = o.id
         WHERE
-          td.id = $1
-          AND td.deleted_at IS NULL 
+          td.deleted_at IS NULL
           AND t.deleted_at IS NULL
         ORDER BY
           r.name
-      `,
-      values: [id]
+      `)
+
+      const data = transactionDetailResult.rows.map(transactionDetail => {
+        const transactions = transactionsResult.rows.filter(transaction => transaction.transaction_id === transactionDetail.id)
+        return {
+          transaction_id: transactionDetail.id,
+          owner_id: transactionDetail.owner_id,
+          owner_name: transactionDetail.owner_name,
+          register_code: transactionDetail.register_code,
+          transaction_date: transactionDetail.transaction_date,
+          transactions: transactions.map(transaction => ({
+            id: transaction.id,
+            resource_name: transaction.resource_name,
+            quantity: transaction.quantity,
+            price: transaction.price
+          }))
+        }
+      })
+      return data
+    } catch (error) {
+      console.log(error)
     }
+  }
 
-    const transactionDetailResult = await this._pool.query(queryTransactionDetail)
-
-    if (!transactionDetailResult.rows.length) throw new NotFoundError('Transaksi tidak ditemukan')
-
-    const transactionResult = await this._pool.query(queryTransactions)
-
-    const data = transactionDetailResult.rows.map(transactionDetail => {
-      const transactions = transactionResult.rows.filter(transaction => transaction.transaction_id === transactionDetail.id)
-      return {
-        transaction_id: transactionDetail.id,
-        owner_id: transactionDetail.owner_id,
-        owner_name: transactionDetail.owner_name,
-        register_code: transactionDetail.register_code,
-        transaction_date: transactionDetail.transaction_date,
-        transactions: transactions.map(transaction => ({
-          id: transaction.id,
-          resource_name: transaction.resource_name,
-          quantity: transaction.quantity,
-          price: transaction.price
-        }))
+  async getTransactionDetailById (id) {
+    try {
+      const queryTransactionDetail = {
+        text: `
+          SELECT DISTINCT
+            td.id,
+            o.id AS owner_id,
+            o.name AS owner_name,
+            o.register_code,
+            td.transaction_date,
+            td.updated_at
+          FROM
+            transaction_details td
+            INNER JOIN transactions t ON td.id = t.transaction_id
+            INNER JOIN med_resources r ON t.resource_id = r.id
+            INNER JOIN owners o ON td.owner_id = o.id
+          WHERE
+            td.id = $1 
+            AND td.deleted_at IS NULL
+            AND t.deleted_at IS NULL
+          ORDER BY
+            td.transaction_date
+        `,
+        values: [id]
       }
-    })
-    return data[0]
+
+      const queryTransactions = {
+        text: `
+          SELECT DISTINCT
+            t.id,
+            t.transaction_id,
+            r.name AS resource_name, 
+            t.quantity,
+            t.price
+          FROM
+            transaction_details td
+            INNER JOIN transactions t ON td.id = t.transaction_id
+            INNER JOIN med_resources r ON t.resource_id = r.id
+            INNER JOIN owners o ON td.owner_id = o.id
+          WHERE
+            td.id = $1
+            AND td.deleted_at IS NULL 
+            AND t.deleted_at IS NULL
+          ORDER BY
+            r.name
+        `,
+        values: [id]
+      }
+
+      const transactionDetailResult = await this._pool.query(queryTransactionDetail)
+
+      if (!transactionDetailResult.rows.length) throw new NotFoundError('Transaksi tidak ditemukan')
+
+      const transactionResult = await this._pool.query(queryTransactions)
+
+      const data = transactionDetailResult.rows.map(transactionDetail => {
+        const transactions = transactionResult.rows.filter(transaction => transaction.transaction_id === transactionDetail.id)
+        return {
+          transaction_id: transactionDetail.id,
+          owner_id: transactionDetail.owner_id,
+          owner_name: transactionDetail.owner_name,
+          register_code: transactionDetail.register_code,
+          transaction_date: transactionDetail.transaction_date,
+          transactions: transactions.map(transaction => ({
+            id: transaction.id,
+            resource_name: transaction.resource_name,
+            quantity: transaction.quantity,
+            price: transaction.price
+          }))
+        }
+      })
+
+      return data[0]
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   async getTransactionDetailByOwnerId (ownerId) {
@@ -238,63 +251,71 @@ class TransactionsService {
   }
 
   async editTransactionDetailById (id, { ownerId }) {
-    const updatedAt = await new GetLocalTime().getDate()
-    const query = {
-      text: `
-        UPDATE
-          transaction_details
-        SET
-          owner_id = $1,
-          updated_at = $2
-        WHERE
-          id = $3
-          AND deleted_at IS NULL
-        RETURNING id
-      `,
-      values: [ownerId, updatedAt, id]
+    try {
+      const updatedAt = await new GetLocalTime().getDate()
+      const query = {
+        text: `
+          UPDATE
+            transaction_details
+          SET
+            owner_id = $1,
+            updated_at = $2
+          WHERE
+            id = $3
+            AND deleted_at IS NULL
+          RETURNING id
+        `,
+        values: [ownerId, updatedAt, id]
+      }
+
+      const result = await this._pool.query(query)
+
+      if (!result.rows.length) throw new NotFoundError('Gagal memperbarui detail transaksi. Id tidak ditemukan')
+    } catch (error) {
+      console.log(error)
     }
-
-    const result = await this._pool.query(query)
-
-    if (!result.rows.length) throw new NotFoundError('Gagal memperbarui detail transaksi. Id tidak ditemukan')
   }
 
   async deleteTransactionDetailById (id) {
-    const deletedAt = await new GetLocalTime().getDate()
-    const queryTransactions = {
-      text: `
-        UPDATE
-          transactions
-        SET
-          deleted_at = $1
-        WHERE
-          transaction_id = $2
-          AND deleted_at IS NULL
-        RETURNING id
-      `,
-      values: [deletedAt, id]
+    try {
+      const deletedAt = await new GetLocalTime().getDate()
+      const queryTransactions = {
+        text: `
+          UPDATE
+            transactions
+          SET
+            deleted_at = $1
+          WHERE
+            transaction_id = $2
+            AND deleted_at IS NULL
+          RETURNING id
+        `,
+        values: [deletedAt, id]
+      }
+
+      const resultTransaction = await this._pool.query(queryTransactions)
+
+      if (!resultTransaction.rows.length) throw new NotFoundError('Gagal menghapus transaksi. Id tidak ditemukan')
+
+      const queryTransactionDetail = {
+        text: `
+          UPDATE
+            transaction_details
+          SET
+            deleted_at = $1
+          WHERE
+            id = $2
+            AND deleted_at IS NULL
+          RETURNING id
+        `,
+        values: [deletedAt, id]
+      }
+      const resultTransDetail = await this._pool.query(queryTransactionDetail)
+
+      if (!resultTransDetail.rows.length) throw new NotFoundError('Gagal menghapus detail transaksi. Id tidak ditemukan.')
+    } catch (error) {
+      console.log(error)
     }
-
-    const resultTransaction = await this._pool.query(queryTransactions)
-
-    if (!resultTransaction.rows.length) throw new NotFoundError('Gagal menghapus transaksi. Id tidak ditemukan')
-
-    const queryTransactionDetail = {
-      text: `
-        UPDATE
-          transaction_details
-        SET
-          deleted_at = $1
-        WHERE
-          id = $2
-          AND deleted_at IS NULL
-        RETURNING id
-      `,
-      values: [deletedAt, id]
-    }
-    const resultTransDetail = await this._pool.query(queryTransactionDetail)
-
-    if (!resultTransDetail.rows.length) throw new NotFoundError('Gagal menghapus detail transaksi. Id tidak ditemukan.')
   }
 
   async addTransaction (transactionId, transactionsData) {
@@ -336,75 +357,87 @@ class TransactionsService {
   }
 
   async getTransactions () {
-    const result = await this._pool.query(`
-      SELECT
-        t.id,
-        t.transaction_id,
-        t.resource_id,
-        r.name AS resource_name,
-        t.quantity,
-        t.price,
-        t.created_at,
-        t.updated_at
-      FROM
-        transactions t
-        INNER JOIN med_resources r ON t.resource_id = r.id
-      WHERE
-        t.deleted_at IS NULL
-        AND r.deleted_at IS NULL
-      ORDER BY
-        t.created_at
-      `)
+    try {
+      const result = await this._pool.query(`
+        SELECT
+          t.id,
+          t.transaction_id,
+          t.resource_id,
+          r.name AS resource_name,
+          t.quantity,
+          t.price,
+          t.created_at,
+          t.updated_at
+        FROM
+          transactions t
+          INNER JOIN med_resources r ON t.resource_id = r.id
+        WHERE
+          t.deleted_at IS NULL
+          AND r.deleted_at IS NULL
+        ORDER BY
+          t.created_at
+        `)
 
-    return result.rows
+      return result.rows
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   async editTransactionById (id, { resourceId, quantity }) {
-    const price = await this.getResourcePriceByResourceId(resourceId)
+    try {
+      const price = await this.getResourcePriceByResourceId(resourceId)
 
-    const updatedAt = await new GetLocalTime().getDate()
+      const updatedAt = await new GetLocalTime().getDate()
 
-    const query = {
-      text: `
-        UPDATE
-          transactions
-        SET 
-          resource_id = $1,
-          quantity = $2,
-          price = $3,
-          updated_at = $4
-        WHERE
-          id = $5
-          AND deleted_at IS NULL
-        RETURNING id
-      `,
-      values: [resourceId, quantity, price, updatedAt, id]
+      const query = {
+        text: `
+          UPDATE
+            transactions
+          SET 
+            resource_id = $1,
+            quantity = $2,
+            price = $3,
+            updated_at = $4
+          WHERE
+            id = $5
+            AND deleted_at IS NULL
+          RETURNING id
+        `,
+        values: [resourceId, quantity, price, updatedAt, id]
+      }
+
+      const result = await this._pool.query(query)
+
+      if (!result.rows.length) throw new NotFoundError('Gagal memperbarui Transaksi. Id transaksi tidak ditemukan')
+    } catch (error) {
+      console.log(error)
     }
-
-    const result = await this._pool.query(query)
-
-    if (!result.rows.length) throw new NotFoundError('Gagal memperbarui Transaksi. Id transaksi tidak ditemukan')
   }
 
   async deleteTransactionById (id) {
-    const deletedAt = await new GetLocalTime().getDate()
-    const query = {
-      text: `
-        UPDATE
-          transactions
-        SET
-          deleted_at = $1
-        WHERE
-          id = $2
-          AND deleted_at IS NULL
-        RETURNING id
-      `,
-      values: [deletedAt, id]
+    try {
+      const deletedAt = await new GetLocalTime().getDate()
+      const query = {
+        text: `
+          UPDATE
+            transactions
+          SET
+            deleted_at = $1
+          WHERE
+            id = $2
+            AND deleted_at IS NULL
+          RETURNING id
+        `,
+        values: [deletedAt, id]
+      }
+
+      const result = await this._pool.query(query)
+
+      if (!result.rows.length) throw new NotFoundError('Gagal menghapus transaksi. Id tidak ditemukan')
+    } catch (error) {
+      console.log(error)
     }
-
-    const result = await this._pool.query(query)
-
-    if (!result.rows.length) throw new NotFoundError('Gagal menghapus transaksi. Id tidak ditemukan')
   }
 }
 
