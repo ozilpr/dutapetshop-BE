41526a5a8fbdc9eb0262c0747f78291894643172
ozilpr <ozilp48@gmail.com>
2024-10-01 15:28:52 +0700

    modify some schema to match new database model

--- src/validator/transactions/schema.js
+++ src/validator/transactions/schema.js
@@ -3,13 +3,19 @@ const Joi = require('joi')
 const TransactionsSchema = Joi.object({
   resourceId: Joi.string().required(),
   quantity: Joi.number().required(),
-  discount: Joi.number().allow(null, ''),
-  discountType: Joi.string().allow(null, '')
+  price: Joi.number().required()
 })
 
 const TransactionsPayloadSchema = Joi.object({
   ownerId: Joi.string().required(),
+  discount: Joi.number().allow(null, ''),
+  totalPrice: Joi.number().required(),
   transactionsData: Joi.array().items(TransactionsSchema).min(1).required()
 })
 
-module.exports = { TransactionsPayloadSchema }
+const TransactionQuerySchema = Joi.object({
+  startDate: Joi.date().allow(null, ''),
+  endDate: Joi.date().allow(null, '')
+})
+
+module.exports = { TransactionsPayloadSchema, TransactionQuerySchema }
